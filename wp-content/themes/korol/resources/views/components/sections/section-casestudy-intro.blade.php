532407439a7a73b6ef0a0bcb94a4fc2da539b53a
Trefor Coster <trefor@savvi.studio>
2022-10-06 15:44:34 +0100

    edit to link

--- wp-content/themes/korol/resources/views/components/sections/section-casestudy-intro.blade.php
+++ wp-content/themes/korol/resources/views/components/sections/section-casestudy-intro.blade.php
@@ -45,7 +45,7 @@
 
           <?php if ($film){ ?>
 
-          <a class="film-link" targe="_blank" href="<?php echo $film;?>">
+          <a class="film-link" target="_blank" href="<?php echo $film;?>">
 
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                  viewBox="0 0 36.6 36.6"  xml:space="preserve">
